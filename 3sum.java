9cd22fdf19d3aa86e6c68c0ebdd361d348c8b67d
Kirtan thakkar <kirtanthakkar30@gmail.com>
2026-06-08 23:03:33 +0530

    Add 3sum solution

---
+++ 3sum.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+       Arrays.sort(nums);
+       List<List<Integer>> result = new ArrayList<>();
+       for(int i=0;i<nums.length-2;i++){
+        int left=i+1,right=nums.length-1;
+
+        if(i>0 && nums[i] == nums[i-1]){
+            continue;
+        }
+        while(left<right){
+            int sum = nums[i] + nums[left] + nums[right];
+            if(sum==0){
+                result.add(Arrays.asList(nums[i],nums[left],nums[right]));
+                left++;
+                right--;
+                while(left<right && nums[left]==nums[left-1]){
+                    left++;
+                }
+            }else if(sum<0){
+                left++;
+            }else{
+                right--;
+            }
+        }
+       }
+       return result;
+    }
+}
