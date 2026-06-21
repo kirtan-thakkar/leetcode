0a4d80fe46e884992b1b385895786c26d195b51f
Kirtan thakkar <kirtanthakkar30@gmail.com>
2026-06-21 15:08:35 +0530

    add reorder list 2

---
+++ fastslowpointer/reorderlist-2.java
@@ -0,0 +1,40 @@
+class Solution {
+    public void reorderList(ListNode head) {
+        if(head == null || head.next == null) return;
+
+        ListNode fast = head;
+        ListNode slow = head;
+
+        // find middle
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // reverse second half
+        ListNode prev = null;
+        ListNode curr = slow;
+
+        while(curr != null){
+            ListNode nextNode = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+
+        // merge both halves
+        ListNode first = head;
+        ListNode second = prev;
+
+        while(second.next != null){
+            ListNode temp1 = first.next;
+            ListNode temp2 = second.next;
+
+            first.next = second;
+            second.next = temp1;
+
+            first = temp1;
+            second = temp2;
+        }
+    }
+}
