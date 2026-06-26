021e4782a62cf57b6755d93f56fbb1466b756ec8
Kirtan thakkar <kirtanthakkar30@gmail.com>
2026-06-26 22:59:35 +0530

    Add peak index in mountain array sol

---
+++ binary-search/peak-index-mountainarray.java
@@ -0,0 +1,43 @@
+class Solution {
+
+    public int peakIndexInMountainArray(int[] arr) {
+
+        if(arr.length==0){
+
+            return 0;
+
+        }
+
+        int low =1; 
+
+        int high = arr.length -2 ;
+
+        int res = -1;
+
+
+
+        while(low <= high){
+
+            int guess = (low +high)/2;
+
+            int prevint = arr[guess-1];
+
+            int nextint = arr[guess+1];
+
+            if(arr[guess] > prevint && arr[guess] > nextint){
+
+                return guess;
+
+            }else if(arr[guess] < nextint ){
+                low = guess + 1;
+            }else{
+                high = guess -1;
+            }
+
+        }
+
+        return res;
+
+    }
+
+}
