e3426aac5545a636745d1cfd8066fe84d733fc68
Kirtan thakkar <kirtanthakkar30@gmail.com>
2026-06-26 22:58:26 +0530

    add first and last pos of an element in an sorted array

---
+++ binary-search/first&lastpos.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int[] res = new int[2];
+        res[0] = -1;
+        res[1] = -1;
+        int low = 0;
+        int high = nums.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] < target) {
+                low = mid + 1;
+            } else if (nums[mid] > target) {
+                high = mid - 1;
+            } else {
+                res[0] = mid;     // Record the match
+                high = mid - 1;   // Keep looking left
+            }
+        }
+
+        // 2. Find the last (rightmost) position
+        low = 0;
+        high = nums.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] < target) {
+                low = mid + 1;
+            } else if (nums[mid] > target) {
+                high = mid - 1;
+            } else {
+                res[1] = mid;     // Record the match
+                low = mid + 1;    // Keep looking right
+            }
+        }
+
+        return res;
+    }
+}
