f3113265d86d8b9711de48b0d43ed5c2cc8779c5
Kirtan thakkar <kirtanthakkar30@gmail.com>
2026-07-02 23:04:36 +0530

    add soln for kth smallest element in sorted matrix

---
+++ binary-search/kth-smallest-element-in-sorted-matrix.java
@@ -0,0 +1,51 @@
+class Solution {
+    int countLessEqual(int[][] matrix, int target) {
+
+        int n = matrix.length;
+
+        int row = n - 1;
+        int col = 0;
+
+        int count = 0;
+
+        while (row >= 0 && col < n) {
+
+            if (matrix[row][col] <= target) {
+
+                count += row + 1;
+                col++;
+
+            } else {
+
+                row--;
+            }
+        }
+
+        return count;
+    }
+
+    public int kthSmallest(int[][] matrix, int k) {
+
+        int n = matrix.length;
+
+        int low = matrix[0][0];
+        int high = matrix[n - 1][n - 1];
+
+        while (low <= high) {
+
+            int guess = (high +low) / 2;
+
+            int count = countLessEqual(matrix, guess);
+
+            if (count < k) {
+                low = guess + 1;
+            } else {
+                high = guess -1;
+            }
+        }
+
+        return low;
+    }
+
+    
+}
