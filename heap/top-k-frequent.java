42093a1a0f25910d861dee6246b1c2dda3d81557
Kirtan thakkar <kirtanthakkar30@gmail.com>
2026-08-01 17:43:28 +0530

    update top k frequent elements code according to the template i learned

--- heap/top-k-frequent.java
+++ heap/top-k-frequent.java
@@ -1,34 +1,56 @@
+class Pair {
+    int first;
+    int second;
+
+    Pair(int first, int second) {
+        this.first = first;
+        this.second = second;
+    }
+}
+
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
 
-        Map<Integer, Integer> map = new HashMap<>();
-        PriorityQueue<int[]> minHeap =
-                new PriorityQueue<>((a, b) -> Integer.compare(a[1], b[1]));
+        HashMap<Integer,Integer> h1 = new HashMap<>();
 
-        int[] ans = new int[k];
-
-        for (int num : nums) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
+        for(int num : nums){
+            h1.put(num , h1.getOrDefault(num,0)+1);
         }
 
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            minHeap.add(new int[]{entry.getKey(), entry.getValue()});
+        //minHeap is used cause that is more optimal by eliminating the elements that does not fit in the top k criteria
+        PriorityQueue<Pair> minHeap = new PriorityQueue<>((a,b)->{
+            if(a.second!=b.second){
+                return a.second - b.second;
+            }
+            return a.first - b.first;
+        });
+        
+        //heap insertion
 
-            if (minHeap.size() > k) {
+        for(Map.Entry<Integer,Integer> entry : h1.entrySet()){
+            
+            int value = entry.getKey();
+            int metric = entry.getValue();
+            minHeap.add(new Pair(value,metric));
+
+            if(minHeap.size() > k){
                 minHeap.poll();
             }
         }
 
+        //amswer
+        int[] ans = new int[k];
         int index = 0;
 
-        while (!minHeap.isEmpty()) {
-            ans[index++] = minHeap.poll()[0];
+        while(!minHeap.isEmpty()){
+            ans[index++] = minHeap.poll().first;
         }
 
         return ans;
+
+
     }
 }
-
 // time complexitcty would O(n+mlogn)
 // frequency map would take O(n) time and processing the heap would take O(mlogn) time
 
